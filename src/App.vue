2008ee15d95d20228123bf620cfa4ffdbc87963d
Piyush <piyushpatil98@protonmail.com>
2021-04-01 19:44:46 +0530

    init Vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,87 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <div class="container">
+            <!-- search -->
+            <div class="row" align="center">
+                <input 
+                type="text" 
+                name="city" 
+                placeholder="Search..." 
+                class="u-full-width"
+                @input="city($event)"
+                />
+                <button type="button" @click="weather">submit</button>
+            </div>
+            <!-- weather -->
+            <div class="row weather" v-if="lock">
+              <h1 class="lock"> {{data.temp}} C </h1>
+              <h5 class="lock"> {{data.city}} </h5>
+              <h4 class="lock"> {{data.cloud}} </h4>
+            </div>
+         
+        </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    data() {
+        return {
+          api_key:'fe000ea81fc76b6904399b3f47ddff19',
+            loc:'',
+            data:{},
+            lock:false,
+        }
+    },
+    methods: {
+      weather(){
+        let data = async ()=> { 
+          let url=`http://api.openweathermap.org/data/2.5/weather?q=${this.loc},in&appid=${this.api_key}`
+          let raw = await ( await fetch(url)).json() 
+          this.data.temp= Math.floor(raw.main.temp-274)
+          this.data.city= raw.name
+          this.data.cloud= raw.weather[0].main
+          this.lock=true
+        }
+        data()
+      },
+      city(e){
+        this.loc=e.target.value
+        console.log(this.loc)
+      }
+    }
 }
 </script>
-
-<style>
+<style> 
+body{
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  height:100vh;
+}
+.container{
+    margin-top: 5em;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    /*background-color: #454356*/
+}
+.row{
+  content:none; 
+}
+.weather{
+  margin-top: 3em;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+
+}
+input{
+  padding: .5em;
+  font-family: Hermit;
+  letter-spacing: 2px;
 }
 </style>
